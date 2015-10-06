952b0bc424135e866cc65fd312fa016aef239cb6
micahcourey <micahjcourey@gmail.com>
2015-10-05 18:11:22 -0700

    add prevSlide and nextSlide methods to add ability to move through slides

--- app.js
+++ app.js
@@ -18,4 +18,12 @@ var imageSlider = angular.module('imageSlider', ['ngAnimate'])
     $scope.isCurrentIndex = function (index) {
       return $scope.currentIndex === index;
     };
+
+    $scope.prevSlide = function (index) {
+      $scope.currentIndex = ($scope.currentIndex < $scope.slide.length - 1) ? ++$scope.currentIndex : 0;
+    };
+
+    $scope.nextSlide = function () {
+      $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slide.length - 1;
+    };
   });
