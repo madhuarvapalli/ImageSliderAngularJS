d18348432f2a3d330c4749368ac87dd4403c7100
micahcourey <micahjcourey@gmail.com>
2015-10-05 18:35:58 -0700

    add TweenMax.min.js to lib folder and add animation method to app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var imageSlider = angular.module('imageSlider', ['ngAnimate'])
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function($scope) {
     $scope.slides = [
       {image:'' description: ''},
       {image:'' description: ''},
@@ -11,19 +11,37 @@ var imageSlider = angular.module('imageSlider', ['ngAnimate'])
 
     $scope.currentIndex = 0;
 
-    $scope.setCurrentIndex = function (index) {
+    $scope.setCurrentIndex = function(index) {
       $scope.currentIndex = index;
     };
 
-    $scope.isCurrentIndex = function (index) {
+    $scope.isCurrentIndex = function(index) {
       return $scope.currentIndex === index;
     };
 
-    $scope.prevSlide = function (index) {
+    $scope.prevSlide = function() {
       $scope.currentIndex = ($scope.currentIndex < $scope.slide.length - 1) ? ++$scope.currentIndex : 0;
     };
 
-    $scope.nextSlide = function () {
+    $scope.nextSlide = function() {
       $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slide.length - 1;
+    };
+  });
+  .animation('slide-animation', function() {
+    return {
+      addClass: function(element, className, done) {
+        if (className == 'ng-hide') {
+
+        } else {
+          done();
+        },
+        removeClass: function(element, className, done) {
+          if (className == 'ng-hide') {
+
+          } else {
+              done();
+          }
+        }
+      }
     };
   });
